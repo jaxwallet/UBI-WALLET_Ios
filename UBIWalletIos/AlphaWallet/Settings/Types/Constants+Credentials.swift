1c261e220b7737c5824064bb9f1586a7b24dc987
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2022-04-27 03:36:34 -0700

    api keys

--- UBIWalletIos/AlphaWallet/Settings/Types/Constants+Credentials.swift
+++ UBIWalletIos/AlphaWallet/Settings/Types/Constants+Credentials.swift
@@ -6,7 +6,7 @@ extension Constants {
     enum Credentials {
         static let infuraKey = "638a8313baad4e02a75b80d45b8889c5"
         static let etherscanKey = "YH711JGVVSJQY7KMMQAI3YXJUE8FSQB44C"
-        static let binanceSmartChainExplorerApiKey = ""
+        static let binanceSmartChainExplorerApiKey: String? = nil
         static let polygonScanExplorerApiKey: String? = nil
         static let analyticsKey = ""
         static let paperTrail = (host: "", port: UInt(0))
@@ -15,5 +15,3 @@ extension Constants {
         static let rampApiKey = "j5wr7oqktym7z69yyf84bb8a6cqb7qfu5ynmeyvn"
     }
 }
-
-
